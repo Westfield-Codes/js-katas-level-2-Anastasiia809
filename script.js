7b1fdf2c8b5e5f1fc044eb732c933fc89f8776e8
Anastasiia809 <anastasiia208805@student-schoolsofwestfield.org>
2025-05-16 14:27:26 +0000

    done shopping list

--- script.js
+++ script.js
@@ -1,67 +1,13 @@
-/* SHOPPING LIST KATA:  */
-
-// Every day, you are sent to buy 5 items. 
-
-/* 1. for loop and string concatenation */
-function main() {
-   makeList();
+/* MULTIPLICATION FREQUENCY KATA */
+function MULTIPLICATION() {
+   let findTopFactor = []
 }
-/* Function makeList()
- * Create a shopping list string called shoppingList as "To Buy:"
- * create a five-item loop, and in the loop, 
- * ask for next item to buy (item #), and add it to the list 
- * add a line break after each item added
- * when loop's done, alert the list
- */
-/* 2. for loop and array with toString */
-
-/* Same function, using previous code, BUT:
- * build shopArray variable for the shopping list
- * adjust loop to work with the array index values (start at 0)
- * ask for next list item and push it to the shopArray
- * no need to add a line break after
- * at end of loop, alert the list title, followed by
- * the list with toString() method (it won't have line breaks)*/
-
-/* 3. for loop and array with join method 
-
-/* Same function, using previous code, BUT:
- * the at end, alert list with array.join() method with line breaks 
- * instead of commas */
- /* 4. 2D Arrays: add Pricing */
-
-/* Same function, using previous code, but THIS TIME add prices
- * When someone enters an item, prompt them also to enter a price separating by comma (like milk, 1.99)])   
- * Convert that entry to an itemPrice array by using string.split() and use a comma as delimiter
- * Push the itemPrice array to shopArray
- */
-
-/* 5. Total up the prices */
-function makeList() {
-   // let total = 0;
-   let shopArray = ["fish", "milk"];
-   // let random =1;
-   // alert(shopArray[random]);
-   let prices = [1.99, 3.55];
-   let question = prompt("Whih item?");
-   let item = shopArray.indexOf(question);
-   alert(prices[item]);
-//    alert(shopArray[random]+prices[shopArray.indexOf]);
-//    let shoppingList = " To Buy: \n";
-//    for (let item = 1;item<=5;item++) {
-//       next = prompt("item "+item+" Enter item name "); 
-//       shopArray.push(next.split(","));
-//    }
-//    for (let item = 0;item<=5;item++) {
-//       total += shopArray[item][1];
-//    }
-//    let message = (shoppingList + shopArray.join("\n"));
-//    message += "Total is " + total;
-//    alert(message);
- }
-/* Same function, using previous code, but THIS TIME total the prices.
- * After you have built the array, create a NEW loop that adds the price 
- * of each item to a total. Do this before the alert. 
- * Save the first alert content as message. 
- * Add total as a line below message, then alert the message. 
+/* findTopFactor(array) analyzes an array of integers from 3-9
+ * Loop over a list, and use another array, factorCount, to 
+ * count how many of those factors appear in the list.  Use a 
+ * loop from 0 to one less than array.length to look at
+ * the next item and push it to the frequency array.  You'll end
+ * up with something like [ 0 1 2 4 2 3 1] for an array [0, 1, 
+ * 3, 4, 3, 4, 2, 4, 5, 4, 5, 6, 1, 2].  Output should be (with 
+ * line breaks) something like: 0:0 1:1 2:2 3:2 4:4) 
  */
